3d54ee82b27ee25597c9063c8d29f958c4b25a02
xiomara <leidyxllc1991@gmail.com>
2023-01-20 16:26:18 -0500

    codigo php

--- app/Http/Controllers/FactorizacionController.php
+++ app/Http/Controllers/FactorizacionController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class FactorizacionController extends Controller
 {
-    //
+
+public function factor(Request $request)
+{
+$factor=array();
+ $numero=2;
+  if(isset($request->numero)){
+    $numero=$request->numero;
+  } 
+$factor = implode(",",$factor);
+return view('factorizacion',compact('factor'));
 }
+}
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>FACTORIZACION</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -20,26 +20,7 @@
                 margin: 0;
             }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
+    
             .content {
                 text-align: center;
             }
@@ -58,9 +39,6 @@
                 text-transform: uppercase;
             }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
@@ -81,7 +59,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    FACTORIZACION
                 </div>
 
                 <div class="links">
