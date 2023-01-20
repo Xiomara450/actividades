7e0304bbdd8e2c57d7567fcae39bcd9c6674da97
xiomara <leidyxllc1991@gmail.com>
2023-01-20 15:19:04 -0500

    diseño de vista

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/factor', function () {
+    return view('factorizacion');
+});
+Route::post('/factor','FactorizacionController@factor')->name ('factor.factorizacion');
